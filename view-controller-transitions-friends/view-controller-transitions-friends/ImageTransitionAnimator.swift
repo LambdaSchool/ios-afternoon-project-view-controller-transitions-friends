fc67b749c832366a7dea010574f70872c280909f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-16 13:47:46 -0700

    animator

---
+++ view-controller-transitions-friends/view-controller-transitions-friends/ImageTransitionAnimator.swift
@@ -0,0 +1,21 @@
+//
+//  ImageTransitionAnimator.swift
+//  view-controller-transitions-friends
+//
+//  Created by Hector Steven on 5/16/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ImageTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+	func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+		return 0.5
+	}
+	
+	func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+		
+	}
+	
+
+}
