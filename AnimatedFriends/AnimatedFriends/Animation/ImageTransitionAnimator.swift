c8a4e9ccabcbaebe47fd9ea6d63a92c2f3c89034
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-16 18:13:49 -0400

    hook up animator

--- AnimatedFriends/AnimatedFriends/Animation/ImageTransitionAnimator.swift
+++ AnimatedFriends/AnimatedFriends/Animation/ImageTransitionAnimator.swift
@@ -18,11 +18,55 @@ class ImageTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
     // MARK: - Delegate Methods
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.5
+        return 1
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        <#code#>
+        guard let toVC = transitionContext.viewController(forKey: .to) as? FriendDetailViewController,
+            let toView = transitionContext.view(forKey: .to) else { return }
+        
+        let containerView = transitionContext.containerView
+        
+        let toViewEndFrame = transitionContext.finalFrame(for: toVC)
+        containerView.addSubview(toView)
+        toView.frame = toViewEndFrame
+        toView.alpha = 0.0
+        
+        sourceNameLabel.alpha = 0.0
+        sourceImageView.alpha = 0.0
+        destinationNameLabel.alpha = 0.0
+        destinationImageView.alpha = 0.0
+        
+        let labelIntialFrame = containerView.convert(sourceNameLabel.bounds, from: sourceNameLabel)
+        let animatedNameLabel = UILabel(frame: labelIntialFrame)
+        animatedNameLabel.text = sourceNameLabel.text
+        animatedNameLabel.font = sourceNameLabel.font
+        containerView.addSubview(animatedNameLabel)
+        
+        let imageInitialFrame = containerView.convert(sourceImageView.bounds, from: sourceImageView)
+        let animatedImageView = UIImageView(frame: imageInitialFrame)
+        animatedImageView.image = sourceImageView.image
+        animatedImageView.contentMode = sourceImageView.contentMode
+        containerView.addSubview(animatedImageView)
+        
+        let duration = transitionDuration(using: transitionContext)
+        toView.layoutIfNeeded()
+        UIView.animate(withDuration: duration, animations: {
+            animatedNameLabel.frame = containerView.convert(self.destinationNameLabel.bounds, from: self.destinationNameLabel)
+            
+            // animatedNameLabel.font.
+            
+            animatedImageView.frame = containerView.convert(self.destinationImageView.bounds, from: self.destinationImageView)
+            toView.alpha = 1.0
+        }) { (_) in
+            self.sourceNameLabel.alpha = 1.0
+            self.sourceImageView.alpha = 1.0
+            self.destinationNameLabel.alpha = 1.0
+            self.destinationImageView.alpha = 1.0
+            animatedNameLabel.removeFromSuperview()
+            animatedImageView.removeFromSuperview()
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
     }
     
 
