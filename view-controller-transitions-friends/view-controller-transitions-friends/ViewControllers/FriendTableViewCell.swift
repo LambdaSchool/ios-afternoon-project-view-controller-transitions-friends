d391979b3baf82d0fa8a4021020b78b254d67ec0
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-16 12:41:46 -0700

    ViewControllers

---
+++ view-controller-transitions-friends/view-controller-transitions-friends/ViewControllers/FriendTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendTableViewCell.swift
+//  view-controller-transitions-friends
+//
+//  Created by Hector Steven on 5/16/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+   
+}
---
+++ view-controller-transitions-friends/view-controller-transitions-friends/ViewControllers/FriendsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsTableViewController.swift
+//  view-controller-transitions-friends
+//
+//  Created by Hector Steven on 5/16/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UITableViewController {
+
+	@IBOutlet var friendImageView: UIImageView!
+	@IBOutlet var nameLabel: UILabel!
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+	}
+	
+}
