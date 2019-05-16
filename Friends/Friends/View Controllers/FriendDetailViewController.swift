499bc699783f0e0e0cffacbac3793b6ac77a9334
Jeffrey Carpenter <spaz926@me.com>
2019-05-16 14:25:14 -0500

    Create initial UI with connections to code

---
+++ Friends/Friends/View Controllers/FriendDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FriendDetailViewController.swift
+//  Friends
+//
+//  Created by Jeffrey Carpenter on 5/16/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+
+    @IBOutlet weak var friendImage: UIImageView!
+    @IBOutlet weak var friendNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
