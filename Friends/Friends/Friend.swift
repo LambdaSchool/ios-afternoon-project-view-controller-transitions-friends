e8fc7a64baeeb4f688b8563f7b1a8b641d09969a
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-23 14:04:51 -0600

    Friend.swift
    Created the model file.

---
+++ Friends/Friends/Friend.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  Friends
+//
+//  Created by Mitchell Budge on 5/23/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let profilePicture: String
+}
